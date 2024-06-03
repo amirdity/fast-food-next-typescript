0b79fc5b12af4382446af26ede93c7a523987b2a
amir fattahi <amirfattahi22@gmail.com>
2024-06-03 16:43:09 +0330

    Add files via upload

---
+++ components/main-header/nav-link.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classes from "./nav-link.module.css"
+function NavLink({ href, children }) {
+  const path = usePathname();
+  return (
+    <Link
+      href={href}
+      className={path.startsWith(href) ? `${classes.Link}  ${classes.active}` : classes.link}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
